d7f55c0ce39fa2d8ef6d5a1712e7103fb0eacc67
0xWaleed <imwaleed@outlook.sa>
2023-09-22 22:54:39 +0300

    init commit :tada:

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+
+game 'common'
+
+server_only 'yes'
+
+server_script 'main.lua'
+
+
---
+++ main.lua
@@ -0,0 +1,63 @@
+local tokens = {}
+
+RegisterCommand('generate-token', function(serverId, args)
+  if serverId ~= 0 then
+    return
+  end
+  local token = math.random(100000, 999999)
+  print(('Token: (%s)'):format(token))
+  tokens[tostring(token)] = true
+end, true)
+
+RegisterCommand('list-tokens',  function(serverId)
+  if serverId ~= 0 then
+    return
+  end
+  print('listing tokens')
+  for token, _ in pairs(tokens) do 
+    print(('token: %s'):format(token))
+  end
+  print('done listing tokens')
+end, true)
+
+AddEventHandler('playerConnecting', function(name, skr, d)
+  local serverId = source
+  print(('Connecting %s.'):format(name))
+
+  d.defer()
+
+  Wait(50)
+
+  function onResponse(data, rawData)
+    local token = data['txtToken']
+
+    if not tokens[token] then
+      d.done('Take a U turn!')
+      return
+    end
+
+    tokens[token] = nil
+    d.done()
+  end
+
+  d.presentCard([[
+  {
+    "type": "AdaptiveCard",
+    "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+    "version": "1.6",
+    "actions": [
+        {
+            "type": "Action.Submit",
+            "title": "Enter"
+        }
+    ],
+    "body": [
+        {
+            "type": "Input.Text",
+            "placeholder": "Placeholder text",
+            "id": "txtToken"
+        }
+    ]
+}
+  ]], onResponse)
+end)
